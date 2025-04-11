e889ad6a51b6974a14883e5d3072f45541e998ef
oKriM-Bitt <mirkoxavier90@gmail.com>
2025-04-11 14:03:46 -0300

    Actualizado Final

--- ClsConexion.cs
+++ ClsConexion.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Data.SqlClient;
 using System.Windows.Forms;
+using System.Data;
 
 namespace PryConexionBDLopezBearzi
 {
@@ -13,6 +14,39 @@ namespace PryConexionBDLopezBearzi
     {
         private static string connectionString =
    "Server=PC221;Database=Comercio;Trusted_Connection=True;";
+
+        public void Conectar(DataGridView GrlM)
+        {
+
+
+            string connectionString = "Server=PC221;Database=Comercio;Trusted_Connection=True;";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = @"SELECT p.Codigo, p.Nombre, p.Descripcion, p.Precio, p.Stock, c.Nombre AS Categoria
+                     FROM Productos p
+                     INNER JOIN Categorias c ON p.CategoriaId = c.Id";
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    // Utilizando un DataTable para almacenar los resultados
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(command);
+                    da.Fill(dt);  // Llenamos el DataTable con los resultados de la consulta
+
+                    // Asignamos el DataTable al DataGridView
+                    GrlM.DataSource = dt; // dataGridView1 es el nombre de tu grilla
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("❌ Error al conectar: " + ex.Message);
+                }
+            }
+        }
+
+
 
         public static SqlConnection ObtenerConexion()
         {
--- Form1.cs
+++ Form1.cs
@@ -19,34 +19,10 @@ namespace PryConexionBDLopezBearzi
         {
             InitializeComponent();
         }
-
+        ClsConexion conexion = new ClsConexion();
         private void BtnMostrar_Click(object sender, EventArgs e)
         {
-            string connectionString = "Server=PC221;Database=Comercio;Trusted_Connection=True;";
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                try
-                {
-                    connection.Open();
-                    string query = @"SELECT p.Codigo, p.Nombre, p.Descripcion, p.Precio, p.Stock, c.Nombre AS Categoria
-                     FROM Productos p
-                     INNER JOIN Categorias c ON p.CategoriaId = c.Id";
-                    SqlCommand command = new SqlCommand(query, connection);
-
-                    // Utilizando un DataTable para almacenar los resultados
-                    DataTable dt = new DataTable();
-                    SqlDataAdapter da = new SqlDataAdapter(command);
-                    da.Fill(dt);  // Llenamos el DataTable con los resultados de la consulta
-
-                    // Asignamos el DataTable al DataGridView
-                    GrlM.DataSource = dt; // dataGridView1 es el nombre de tu grilla
-
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("❌ Error al conectar: " + ex.Message);
-                }
-            }
+          conexion.Conectar(GrlM);
         }
 
         private void GrlM_CellContentClick(object sender, DataGridViewCellEventArgs e)
